952f3a66b3edd1982e647b4ac6d9158aaa6692b7
Sandunga <sandra.cuevastorres@gmail.com>
2019-01-31 10:27:04 -0600

    ya sirve prompt y todavia no sirve el cipher pero ya existe

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,45 @@
-window.cipher = {
-  // ... 
+window.cipher ={
+
+encode (offset, msgInput){
+//declaro el alfabeto
+  let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  //para saber el largo del alfabeto
+  let alphabetSize = alphabet.length;
+  //aquí se va ir generarndo el mensaje codificado
+  let msgEncode='';
+  //para codificar el mensaje que ingresa el usuario
+  //donde i=0 es la posición de inicio, i es el lugar donde se encuentra
+  for (let i=0; i<= msgInput.lenght-1; i++);
+  if (msgInput[1]==''){
+    msgEncode += '';
+
+  let position = (msgInput.charCodeAt(i)-'A'.charCodeAt(0)+offset)%alphabetSize;
+  msgEncode += alphabet[position];
+    }
+return msgEncode;
+
+},
+
+decode (offset, msgInput){
+   //declaro el alfabeto
+     let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+     //para saber el largo del alfabeto
+     let alphabetSize = alphabet.length;
+     //aquí se va ir generarndo el mensaje codificado
+     let msgEncode='';
+
+     //para codificar el mensaje que ingresa el usuario
+     //donde i=0 es la posición de inicio, i es el lugar donde se encuentra
+     for (let i=0; i<= msgInput.lenght-1; i++);
+     if (msgInput[1]==''){
+       msgEncode += '';
+
+     let position = (msgInput.charCodeAt(i)-'A'.charCodeAt(0)+offset)%alphabetSize;
+
+     msgEncode += alphabet[position];
+}
+     }
+   return msgEncode
+
+      }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 
+  const userName = prompt("¡Bienvenid@ a " + " 'LA CASA DE LOS SECRETOS' " + "Ingresa tu nombre para jugar:");
+
+  const secretear = alert(userName + " Vamos a secretear!");
+
+
+
+//aquí esta jalando los inputs del index html
+function getInput (){
+// msgInput jala el mensaje que ingresa el usuario, desde html
+  const msgInput = document.getElementById('input').value;
+  //jala el numero de posiciones que recorrera, desde el html
+  const msgOffset = document.getElementById('claveCifrado').value;
+
+  console.log(msgInput);
+  console.log(msgOffset);
+// jala la variable en donde se depositará el mensaje que ingresa el usuario, del cipher.js
+    let msgEncode = encode(msgOffset, msgInput);
+
+
+  console.log(msgEncode);
+
+}
